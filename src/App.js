814353c20e84fb7d16896e460b7c4650071e0808
Adam Andrews <andrewsadam07@gmail.com>
2022-03-07 11:33:28 +0000

    Added convert funciton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { convert  } from './binary';
 
 function App() {
+  const [binary,setBinary] = useState("")
+  const [number,setNumber] = useState("")
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input type="number" value={binary} onChange={(e)=> setBinary(e.target.value)}></input>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>
+            {binary} {number}
+          </code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=> setNumber(convert(binary.split("")))}>
+          Conver binary to decimal
+        </button>
       </header>
     </div>
   );
---
+++ src/binary.js
@@ -0,0 +1,18 @@
+export function convert(arr) {
+    const rev = arr.reverse()
+    let value = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (rev[i] === 1){ 
+            value += 2**i
+        }
+        else if (rev[i] !== 0) {
+
+        value = "ERROR"
+        break
+      }
+    }
+    return value
+}
+
+const result = convert([1,1,1,1,0])
+console.log(result)
